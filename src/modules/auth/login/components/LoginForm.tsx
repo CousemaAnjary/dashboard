5261183e003008b9e98a9c31a17e468f2f78c103
CousemaAnjary <anjaryabdillah@gmail.com>
2025-01-19 00:20:09 +0300

    Mise à jour des styles de police dans le formulaire de connexion pour utiliser la police Space Grotesk

--- src/modules/auth/login/components/LoginForm.tsx
+++ src/modules/auth/login/components/LoginForm.tsx
@@ -17,11 +17,11 @@ export default function LoginForm() {
     return (
         <>
             <div className="relative p-8 w-full max-w-md">
-                <h1 className="mb-2 text-2xl font-medium font-inter text-black">Se connecter</h1>
+                <h1 className="mb-2 text-2xl font-medium font-spaceGrotesk text-black">Se connecter</h1>
                 <p className="mb-4 text-md text-black">
                     Connectez-vous à votre compte pour accéder à votre espace personnel
                 </p>
-                <p className="mb-4 text-sm  text-muted-foreground">
+                <p className="mb-4 text-sm  font-spaceGrotesk font-medium text-muted-foreground">
                     Vous n&apos;avez pas de compte ? Inscrivez-vous en cliquant <Link href="/auth/register" className="underline text-cyan-700">ici</Link>
                 </p>
             </div>
