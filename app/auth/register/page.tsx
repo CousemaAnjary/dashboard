14ab6f2fee173959bf955997d762b8393dc3dac6
CousemaAnjary <anjaryabdillah@gmail.com>
2025-01-18 15:46:52 +0300

    Suppression de la classe de fond bg-slate-50 dans les composants de la page d'enregistrement et de la page principale pour une meilleure cohérence visuelle

--- app/auth/register/page.tsx
+++ app/auth/register/page.tsx
@@ -3,6 +3,7 @@ import Navbar from "@/src/components/navbar"
 import GridPattern from "@/src/components/ui/grid-pattern"
 
 
+
 export default function Register() {
     /**
      * ! STATE (état, données) de l'application
@@ -18,8 +19,8 @@ export default function Register() {
      * ! AFFICHAGE (render) de l'application
      */
     return (
-        <div className="flex flex-col min-h-screen bg-slate-50">
-             <GridPattern width={50} height={50} x={-1} y={-1} strokeDasharray={"4 2"} className={cn("absolute inset-0 [mask-image:radial-gradient(900px_circle_at_center,white,transparent)]")} />
+        <div className="flex flex-col min-h-screen ">
+            <GridPattern width={50} height={50} x={-1} y={-1} strokeDasharray={"4 2"} className={cn("absolute inset-0 [mask-image:radial-gradient(900px_circle_at_center,white,transparent)]")} />
             {/* En-tête */}
             <header>
                 <Navbar />
@@ -30,9 +31,7 @@ export default function Register() {
                 {/* Section 1 */}
                 <section className="flex justify-center items-center min-h-[84vh]">
                     {/*  container du formulaire d'inscription */}
-                    <div>
-
-                    </div>
+             
                 </section>
             </main>
 
--- app/page.tsx
+++ app/page.tsx
@@ -18,7 +18,7 @@ export default function Page() {
    * ! AFFICHAGE (render) de l'application
    */
   return (
-    <div className="flex flex-col min-h-screen bg-slate-50">
+    <div className="flex flex-col min-h-screen">
       <GridPattern width={50} height={50} x={-1} y={-1} strokeDasharray={"4 2"} className={cn("absolute inset-0 [mask-image:radial-gradient(900px_circle_at_center,white,transparent)]")} />
       {/* En-tête */}
       <header>
@@ -27,8 +27,7 @@ export default function Page() {
 
       {/* Contenu principal */}
       <main className="flex-grow">
-        <h1>Hello Next.js!</h1>
-        <p>Welcome to your first Next.js site.</p>
+      
       </main>
 
       {/* Pied de page */}
