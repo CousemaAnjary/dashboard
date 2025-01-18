ebac267aab460a2b9da7a1c0bfe6f11743b39dd8
CousemaAnjary <anjaryabdillah@gmail.com>
2025-01-18 15:34:34 +0300

    Ajout du composant GridPattern et de la barre de navigation à la page principale pour améliorer la structure et le rendu

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
-import { Button } from "@/src/components/ui/button"
-
+import { cn } from "@/src/lib/utils"
+import Navbar from "@/src/components/navbar"
+import GridPattern from "@/src/components/ui/grid-pattern"
 
 
 export default function Page() {
@@ -17,11 +18,21 @@ export default function Page() {
    * ! AFFICHAGE (render) de l'application
    */
   return (
-    <>
-      <h1>Hello Next.js!</h1>
-      <p>Welcome to your first Next.js site.</p>
+    <div className="flex flex-col min-h-screen bg-slate-50">
+      <GridPattern width={50} height={50} x={-1} y={-1} strokeDasharray={"4 2"} className={cn("absolute inset-0 [mask-image:radial-gradient(900px_circle_at_center,white,transparent)]")} />
+      {/* En-tête */}
+      <header>
+        <Navbar />
+      </header>
 
-      <Button>Click me</Button>
-    </>
+      {/* Contenu principal */}
+      <main className="flex-grow">
+        <h1>Hello Next.js!</h1>
+        <p>Welcome to your first Next.js site.</p>
+      </main>
+
+      {/* Pied de page */}
+      <footer></footer>
+    </div >
   )
 }
