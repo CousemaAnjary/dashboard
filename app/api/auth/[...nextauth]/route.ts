b2e59a29355e6e6de2282a731dc247cad56b24ee
CousemaAnjary <anjaryabdillah@gmail.com>
2025-01-19 23:34:25 +0300

    Refactor l'authentification : nettoyage du code et réorganisation des imports dans les fichiers de gestion de NextAuth

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,21 +1,6 @@
-import NextAuth from "next-auth";
-import { authOptions } from "@/src/lib/auth";
+import NextAuth from "next-auth"
+import { authOptions } from "@/src/lib/auth"
 
-const handler = NextAuth(authOptions);
+const handler = NextAuth(authOptions)
 
-export { handler as GET, handler as POST };
-
-
-// import { PrismaAdapter } from '@next-auth/prisma-adapter';
-// import { prisma } from '@/src/lib/prisma'
-// import NextAuth, { NextAuthOptions } from 'next-auth'
-
-
-// export const authConfig = {
-//     providers: [
-//         GithubProvider()
-//     ],
-//     adapter: PrismaAdapter(prisma),
-// } satisfies NextAuthOptions;
-
-// export default NextAuth(authConfig);
+export { handler as GET, handler as POST }
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,24 +1,8 @@
-// import { NextAuthOptions } from "next-auth";
-// import GitHubProvider from "next-auth/providers/github";
+import { prisma } from "@/src/lib/prisma"
+import { NextAuthOptions } from "next-auth"
+import GitHubProvider from "next-auth/providers/github"
+import { PrismaAdapter } from "@next-auth/prisma-adapter"
 
-// export const authOptions: NextAuthOptions = {
-//   providers: [
-//     GitHubProvider({
-//       clientId: process.env.GITHUB_CLIENT_ID!,
-//       clientSecret: process.env.GITHUB_CLIENT_SECRET!,
-//     }),
-//   ],
-//   // Ajoutez des callbacks ou des configurations supplémentaires ici si nécessaire
-// };
-
-// export default authOptions;
-
-
-
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import { prisma } from "@/src/lib/prisma";
-import GitHubProvider from "next-auth/providers/github";
-import { NextAuthOptions } from "next-auth";
 
 export const authOptions: NextAuthOptions = {
     providers: [
@@ -28,5 +12,5 @@ export const authOptions: NextAuthOptions = {
         }),
     ],
     adapter: PrismaAdapter(prisma),
-    secret: process.env.NEXTAUTH_SECRET,
-};
+    // secret: process.env.NEXTAUTH_SECRET,
+}
