688513a691c2dfb833cd947edbc6c2a0844ca81f
CousemaAnjary <anjaryabdillah@gmail.com>
2025-01-20 22:56:46 +0300

    Ajout de la configuration des domaines d'images pour les avatars dans next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  reactStrictMode: true,
+  images: {
+    // Fornisseur d'images
+    domains: [
+      "avatars.githubusercontent.com", //GitHub avatar
+      "lh3.googleusercontent.com", //Google avatar
+      "cdn.discordapp.com", //Discord avatar
+      "pbs.twimg.com", //Twitter avatar
+    ],
+
+  },
+}
 
 export default nextConfig;
+
+// 
