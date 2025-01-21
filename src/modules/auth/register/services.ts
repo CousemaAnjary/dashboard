45c2d9873a9109ea24de69c75e184d8693c03e99
CousemaAnjary <anjaryabdillah@gmail.com>
2025-01-21 16:31:21 +0300

    Ajout du service d'inscription avec validation des données et gestion des erreurs

---
+++ src/modules/auth/register/services.ts
@@ -0,0 +1,26 @@
+"use server"
+import { z } from "zod"
+import { registerSchema } from "@/src/lib/validations/auth"
+
+
+export async function register(data: z.infer<typeof registerSchema>): Promise<{ success: boolean, message: string }> {
+    try {
+        // Envoi des données au serveur (API)
+        const response = await fetch("/api/auth/register", {
+            method: "POST",
+            body: JSON.stringify(data),
+        })
+
+        // Récupération de la réponse du serveur
+        const apiResponse = await response.json()
+        return {
+            success: apiResponse.success,
+            message: apiResponse.message
+        }
+
+
+    } catch (error) {
+        console.error(error)
+        throw new Error("Une erreur s'est produite lors de l'inscription.")
+    }
+}
