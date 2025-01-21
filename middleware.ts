cb2dafb3987477a677854cd78ca4dd3ec0e26a7c
CousemaAnjary <anjaryabdillah@gmail.com>
2025-01-21 22:25:06 +0300

    Ajout d'un middleware pour la validation du token de session et redirection vers la page de connexion

---
+++ middleware.ts
@@ -0,0 +1,31 @@
+import { prisma } from './src/lib/prisma'
+import { NextResponse, NextRequest } from 'next/server'
+
+
+
+// This function can be marked `async` if using `await` inside
+export async function middleware(request: NextRequest) {
+
+    // Get the session token from the request
+    const sessionToken = request.cookies.get("sessionToken")?.value
+
+    // Vérifier si le sessionToken est valide
+    if (sessionToken) {
+        const session = await prisma.session.findUnique({
+            where: { sessionToken },
+        })
+
+        // Si la session est valide et non expirée
+        if (session && session.expires > new Date()) {
+            return NextResponse.next()
+        }
+    }
+
+
+    return NextResponse.redirect(new URL('/auth/login', request.url))
+}
+
+// Appliquer le middleware sur les routes protégées
+export const config = {
+    matcher: '/dashboard',
+}
